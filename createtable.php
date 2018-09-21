ef3e626e8bf9dc7046abe03d1a7df12611ae8992
Kartikeya P. Malimath <kartikeyapm@gmail.com>
2018-09-21 12:54:21 +0530

    create table  commented

--- createtable.php
+++ createtable.php
@@ -1,21 +1,23 @@
 <?php
 
+//Admin one time use only*
+
 include ('db/database.php');
 
-$create = "CREATE TABLE `course` 
-( `id` BIGINT(20) NOT NULL AUTO_INCREMENT ,  `uname` VARCHAR(10) NOT NULL ,  
-`name` VARCHAR(30) NOT NULL ,  `team` INT(3) NOT NULL ,  `coursedone` 
-VARCHAR(300) NOT NULL ,  `mockdone` VARCHAR(300) NOT NULL , `date` DATE NOT NULL, 
-`time` TIME NOT NULL,   
- PRIMARY KEY  (`id`)) ENGINE = InnoDB;";
+//$create = "CREATE TABLE `course` 
 
-$results = mysqli_query($con, $create);
-if ($con->query($results) === TRUE) {
+//( `id` BIGINT(20) NOT NULL AUTO_INCREMENT ,  `uname` VARCHAR(10) NOT NULL ,  
+//`name` VARCHAR(30) NOT NULL ,  `team` INT(3) NOT NULL ,  `coursedone` 
+//VARCHAR(300) NOT NULL ,  `mockdone` VARCHAR(300) NOT NULL , `date` DATE NOT NULL, 
+//`time` TIME NOT NULL,   
+// PRIMARY KEY  (`id`)) ENGINE = InnoDB;";
     
-echo "The tables have been created";
-} else {
-    echo "Error updating record: " . $con->error;
-}
+//$results = mysqli_query($con, $create);
+//if ($con->query($results) === TRUE) {
+//echo "The tables have been created";
+//} else {
+//    echo "Error updating record: " . $con->error;
+//}
 
 
 
